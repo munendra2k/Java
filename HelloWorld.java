8c1ae7289b60d2b2f6caff1832c3f287289673df
Munendra kumar <155895092+munendra2k@users.noreply.github.com>
2025-05-23 13:58:01 +0530

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("create a new branch");
-        System.out.println("congratulatins");
-    }
-}
