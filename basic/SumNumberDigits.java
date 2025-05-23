f1a55dd63971976fc732d8318593b8258ae6f568
Munendra Kumar <munendra2k@gmail.com>
2025-05-23 13:36:50 +0530

    add SumNumberDigits.java file

--- basic/SumNumberDigits.java
+++ basic/SumNumberDigits.java
@@ -1,1 +1,16 @@
+public class SumNumberDigits{
+    private static int sumDigits(int num) {
+        int sum=0;
 
+        while (num>0) {
+            sum+= num%10;
+            num/=10;
+        }
+        return sum;
+
+    }
+    public static void main(String[] args) {
+        int num=11;
+        System.out.println(sumDigits(num));
+    }
+}
