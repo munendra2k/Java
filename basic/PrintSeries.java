1ced08f5ad3ead6d967c035b7206ec0e4afd7e9d
Munendra kumar <munendra2k@gmail.com>
2025-05-27 15:58:00 +0530

    add printseries.java file

---
+++ basic/PrintSeries.java
@@ -0,0 +1,11 @@
+public class PrintSeries {
+    private static void printSeries(int num) {
+        for (int i = 0; i < num; i++) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        printSeries(10);
+    }
+}
